64d66843a1decc5edeb328fb451402ae0b456a60
Kingsley Arinzechukwu <110477494+ArthurKingDev@users.noreply.github.com>
2022-11-30 02:12:11 +0100

    Update models.py

--- musicapp/models.py
+++ musicapp/models.py
@@ -7,27 +7,25 @@ class Artiste(models.Model):
     first_name = models.CharField(max_length=12)
     last_name = models.CharField(max_length=12)
     age = models.IntegerField(default=1)
-    song = models.ForeignKey('Song', blank=True, null=True, on_delete=models.CASCADE)
+    
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
 
 class Song(models.Model):
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     date_published = models.DateField(default=datetime.date.today)
     likes = models.IntegerField()
-    artiste_id = models.CharField(max_length=20)
-    lyric = models.ForeignKey('Lyric', blank=True, null=True, on_delete=models.SET_NULL)
+    
     def __str__(self):
         return self.title
     
-    
-
 class Lyric(models.Model):
-    content = models.CharField(max_length=800)
-    song_id = models.CharField(max_length=20)
-    artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.CharField(max_length=4000)
 
     def __str__(self):
         return self.content
+   
     
